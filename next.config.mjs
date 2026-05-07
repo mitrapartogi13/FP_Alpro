c16d3358dd0bb0590b611e977cb9d7728b028e96
Mitra Partogi <mitrapartogi@gmail.com>
2026-05-07 23:05:06 +0700

    gas

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  // reactCompiler: true,
   serverExternalPackages: ["pdf-parse", "@napi-rs/canvas"],
 };
 
